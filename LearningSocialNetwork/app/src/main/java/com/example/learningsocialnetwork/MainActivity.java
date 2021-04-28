474c44f2a07f64dab5105eb7919d281cec054311
AdamAevi <adam@aevisolutions.com>
2021-04-27 21:06:37 -0400

    config [Learning Social Network] - Adding Toast messages to navigation bar

--- LearningSocialNetwork/app/src/main/java/com/example/learningsocialnetwork/MainActivity.java
+++ LearningSocialNetwork/app/src/main/java/com/example/learningsocialnetwork/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.learningsocialnetwork;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private RecyclerView recyclerView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                UserMenuSelector(item);
+                return false;
+            }
+        });
+    }
+
+    private void UserMenuSelector(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_profile:
+                Toast.makeText(this,"Viewing Profile", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_home:
+                Toast.makeText(this,"Going Home", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_friends:
+                Toast.makeText(this,"Opening Friends List", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_find_friends:
+                Toast.makeText(this,"Finding Friends", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_messages:
+                Toast.makeText(this,"Opening Messages", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText(this,"Accessing Settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this,"Logging you out", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
     }
 }
