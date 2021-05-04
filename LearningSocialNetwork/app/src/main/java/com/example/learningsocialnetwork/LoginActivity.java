9cc09413c8abb5b4abea13080d2d37143792439f
AdamAevi <adam@aevisolutions.com>
2021-05-03 21:47:05 -0400

    chore (UI): [FullCircleApp] Updating name of id for Create Acct Button, also adding logic to use Need new account button

--- LearningSocialNetwork/app/src/main/java/com/example/learningsocialnetwork/LoginActivity.java
+++ LearningSocialNetwork/app/src/main/java/com/example/learningsocialnetwork/LoginActivity.java
@@ -2,13 +2,45 @@ package com.example.learningsocialnetwork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.sip.SipSession;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private EditText userEmail, userPassword;
+    private TextView needNewAccountLink;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        needNewAccountLink = (TextView) findViewById(R.id.register_account_link);
+        userEmail = (EditText) findViewById(R.id.login_email);
+        userPassword = (EditText) findViewById(R.id.login_password);
+        loginButton = (Button) findViewById(R.id.login_create_account_button);
+
+        needNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendUserToRegisterActivity();
+            }
+        });
+    }
+
+    private void sendUserToRegisterActivity() {
+
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
+        finish();
     }
 }
